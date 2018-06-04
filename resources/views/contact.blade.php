32a216ab46d351ef7fa8509dee11150d9d4050a1
keith(MacBookPro) <ka33145@googlemail.com>
2018-06-04 23:53:21 +0100

    Added contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('description')
+    Give us a call if we can help you with related advice or help with key digital leadership hires.
+@endsection
+
+@section('main')
+<div class="hero contact-image">
+			
+        <div class="section group">
+            <div class="col span_3">
+                <h1>Get <br>connected</h1>	
+                <p>Give us a call if we can help you with related advice or help with key digital leadership hires.</p>
+                <p class="phone-landscape-hide"><span>Amy Witchalls</span> <br>Executive Assistant <br>+44 (0) 203 865 1701 <br><a href="mailto:info@princedalepartners.com" onClick="ga('send', 'event', 'lead', 'amyemail', 'contactpage', 5, {'NonInteraction': 1});">info@princedalepartners.com</a></p>
+                <div class="phone-landscape">
+                    <h4>Contact Us</h4>
+                    <p>3rd Floor, <br>90 Westbourne Grove, <br> London, W2 5RT</p>
+                    <p><span>Amy Witchalls</span> <br>Executive Assistant <br><a href="tel:+447792176959" onClick="ga('send', 'event', 'lead', 'amyphone', 'contactpage', 10, {'NonInteraction': 1});">+44 (0) 7792 176 959 </a> <br><a href="mailto:info@princedalepartners.com">info@princedalepartners.com</a></p>					
+                </div><!-- phone-landscape -->
+            </div>
+            <a href="https://www.google.co.uk/maps/place/90+Westbourne+Grove,+London+W2+5RT/@51.5153603,-0.202371,15z/data=!4m5!3m4!1s0x48761001f9736d07:0x206de62369679be8!8m2!3d51.515357!4d-0.1936109" target="_blank" class="button">&gt; View our location on Google Maps</a>
+        </div>
+        
+    </div><!-- hero -->
+@endsection
