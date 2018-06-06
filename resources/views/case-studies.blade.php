a9b17dcee74be149cf800bff64b7ab55d1c3e427
keith(MacBookPro) <ka33145@googlemail.com>
2018-06-06 17:51:49 +0100

    Updated page structure

--- resources/views/case-studies.blade.php
+++ resources/views/case-studies.blade.php
@@ -5,108 +5,91 @@
 @endsection
 
 @section('main')
-<div class="hero case-study-image">
-			
-        <div class="section group">
-            <div class="col span_3">
-                <h1>Job done</h1>	
-                <p>We are proud to work with some of the best known brands in the world as well as high profile growth companies. We also thrive off helping individuals with their career and placing them into opportunities where they flourish.</p>
-            </div>
+<div class='row'>
+        <div class='col-12'> 
+            <div class="hero case-study-image">
+                <div class="row">
+                    <div class='col-12 col-md-8 offset-md-2 cover-text'>
+                        <h1>Job done</h1>	
+                        <p>We are proud to work with some of the best known brands in the world as well as high profile growth companies. We also thrive off helping individuals with their career and placing them into opportunities where they flourish.</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class='col-6 offset-3'>
+                        <h2>Example Searches</h2>
+                        <p class='d-block d-md-none'>Click on a logo to read a full case study</p>
+                    </div>
+                </div>
+    
+                <div class='row'>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/coca-cola.html">
+                                <img src="img/logo-coca-cola.png" alt="Coca Cola Logo">
+                            </a>
+                            <a href="case-studies/coca-cola.html" class='d-none d-md-inline'>
+                                <span>Digital Director</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/thomas-cook.html">
+                                <img src="img/logo-thomas-cook.png" alt="Thomas Cook Logo">
+                            </a>
+                            <a href="case-studies/thomas-cook.html" class='d-none d-md-inline'>
+                                <span>Group Head, Digital Experience</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/holland-barrett.html">
+                                <img src="img/logo-holland-barrett.png" alt="Holland and Barrett Logo">
+                            </a>
+                            <a href="case-studies/holland-barrett.html" class='d-none d-md-inline'>
+                                <span>Director, Digital/Ecommerce</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/body-shop.html">
+                                <img src="img/logo-body-shop.png" alt="Body Shop Logo">
+                            </a>
+                            <a href="case-studies/body-shop.html" class='d-none d-md-inline'>
+                                <span>Chief Digital Officer</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/zalando.html">
+                            <img src="img/logo-zalando.png" alt="Zalando Logo">
+                        </a>
+                        <a href="case-studies/zalando.html" class='d-none d-md-inline'>
+                            <span>UK Country Manager</span><br>&gt; Read full case study
+                        </a>
+                    </div>	
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/photobox.html">
+                            <img src="img/logo-photobox.png" alt="Photobox Logo">
+                        </a>
+                        <a href="case-studies/photobox.html" class='d-none d-md-inline'>
+                            <span>Director, Customer Insight</span><br>&gt; Read full case study
+                        </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/secret-escapes.html">
+                            <img src="img/logo-secret-escapes.png" alt="Secret Escapes Logo">
+                        </a>
+                        <a href="case-studies/secret-escapes.html" class='d-none d-md-inline'>
+                            <span>Merchandising Director</span><br>&gt; Read full case study
+                        </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/audio-network.html">
+                            <img src="img/logo-audio-network.png" alt="The Audio Network Logo">
+                        </a>
+                        <a href="case-studies/audio-network.html" class='d-none d-md-inline'>
+                            <span>Product Director</span><br>&gt; Read full case study
+                        </a>
+                    </div>			
+                </div> 
+            </div>     
         </div>
-        
-        <div class="desktop-logos">
-        
-            <div class="section group">
-                <h2>Example Searches</h2>
-                <p class="case-study-line-hide">Click on a logo to read a full case study</p>
-            </div>
-            
-            <div class="section group logos">
-                <div>
-                    <a href="case-studies/coca-cola.html"><img src="img/logo-coca-cola.png" alt="Coca Cola Logo"></a>
-                    <a href="case-studies/coca-cola.html" class="case-study-title-hide"><span>Digital Director</span><br>&gt; Read full case study</a>
-                </div>	
-                <div class="logo-adjust">
-                    <a href="case-studies/thomas-cook.html"><img src="img/logo-thomas-cook.png" alt="Thomas Cook Logo"></a>
-                    <a href="case-studies/thomas-cook.html" class="case-study-title-hide"><span>Group Head, Digital Experience</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/holland-barrett.html"><img src="img/logo-holland-barrett.png" alt="Holland and Barrett Logo"></a>
-                    <a href="case-studies/holland-barrett.html" class="case-study-title-hide"><span>Director, Digital/Ecommerce</span><br>&gt; Read full case study</a>
-                </div>	
-                <div>
-                    <a href="case-studies/body-shop.html"><img src="img/logo-body-shop.png" alt="Body Shop Logo"></a>
-                    <a href="case-studies/body-shop.html" class="case-study-title-hide"><span>Chief Digital Officer</span><br>&gt; Read full case study</a>
-                </div>											
-            </div><!-- logo-line-1 -->
-            
-            <div class="section group logos">
-                <div>
-                    <a href="case-studies/zalando.html"><img src="img/logo-zalando.png" alt="Zalando Logo"></a>
-                    <a href="case-studies/zalando.html" class="case-study-title-hide"><span>UK Country Manager</span><br>&gt; Read full case study</a>
-                </div>	
-                <div class="logo-adjust">
-                    <a href="case-studies/photobox.html"><img src="img/logo-photobox.png" alt="Photobox Logo"></a>
-                    <a href="case-studies/photobox.html" class="case-study-title-hide"><span>Director, Customer Insight</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/secret-escapes.html"><img src="img/logo-secret-escapes.png" alt="Secret Escapes Logo"></a>
-                    <a href="case-studies/secret-escapes.html" class="case-study-title-hide"><span>Merchandising Director</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/audio-network.html"><img src="img/logo-audio-network.png" alt="The Audio Network Logo"></a>
-                    <a href="case-studies/audio-network.html" class="case-study-title-hide"><span>Product Director</span><br>&gt; Read full case study</a>
-                </div>								
-            </div><!-- logo-line-2 -->			
-            
-        </div><!-- desktop-logos -->			
-                    
-        
-    </div><!-- hero -->
-    
-    <div class="phone-logos">
-    
-        <div class="section group">
-            <h2>Example Searches</h2>
-        </div>		
-        
-        <div class="section group logos">
-            <div>
-                <a href="case-studies/coca-cola.html"><img src="img/logo-coca-cola.png" alt="Coca Cola Logo"></a>
-                <a href="case-studies/coca-cola.html"><span>Digital Director</span><br>&gt; Read full case study</a>
-            </div>	
-            <div class="logo-adjust">
-                <a href="case-studies/thomas-cook.html"><img src="img/logo-thomas-cook.png" alt="Thomas Cook Logo"></a>
-                <a href="case-studies/thomas-cook.html"><span>Group Head, Digital Experience</span><br>&gt; Read full case study</a>
-            </div>
-            <div>
-                <a href="case-studies/holland-barrett.html"><img src="img/logo-holland-barrett.png" alt="Holland and Barrett Logo"></a>
-                <a href="case-studies/holland-barrett.html"><span>Director, Digital/Ecommerce</span><br>&gt; Read full case study</a>
-            </div>	
-            <div>
-                <a href="case-studies/body-shop.html"><img src="img/logo-body-shop.png" alt="Body Shop Logo"></a>
-                <a href="case-studies/body-shop.html"><span>Chief Digital Officer</span><br>&gt; Read full case study</a>
-            </div>											
-        </div><!-- logo-line-1 -->
-        
-        <div class="section group logos">
-            <div>
-                <a href="case-studies/zalando.html"><img src="img/logo-zalando.png" alt="Zalando Logo"></a>
-                <a href="case-studies/zalando.html"><span>UK Country Manager</span><br>&gt; Read full case study</a>
-            </div>	
-            <div class="logo-adjust">
-                <a href="case-studies/photobox.html"><img src="img/logo-photobox.png" alt="Photobox Logo"></a>
-                <a href="case-studies/photobox.html"><span>Director, Customer Insight</span><br>&gt; Read full case study</a>
-            </div>
-            <div>
-                <a href="case-studies/secret-escapes.html"><img src="img/logo-secret-escapes.png" alt="Secret Escapes Logo"></a>
-                <a href="case-studies/secret-escapes.html"><span>Merchandising Director</span><br>&gt; Read full case study</a>
-            </div>
-            <div>
-                <a href="case-studies/audio-network.html"><img src="img/logo-audio-network.png" alt="The Audio Network Logo"></a>
-                <a href="case-studies/audio-network.html"><span>Product Director</span><br>&gt; Read full case study</a>
-            </div>								
-        </div><!-- logo-line-2 -->			
-        
-    </div><!-- phone-logos -->
+    </div>	
+
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,21 +5,25 @@
 @endsection
 
 @section('main')
-<div class="hero contact-image">
-			
-        <div class="section group">
-            <div class="col span_3">
-                <h1>Get <br>connected</h1>	
-                <p>Give us a call if we can help you with related advice or help with key digital leadership hires.</p>
-                <p class="phone-landscape-hide"><span>Amy Witchalls</span> <br>Executive Assistant <br>+44 (0) 203 865 1701 <br><a href="mailto:info@princedalepartners.com" onClick="ga('send', 'event', 'lead', 'amyemail', 'contactpage', 5, {'NonInteraction': 1});">info@princedalepartners.com</a></p>
-                <div class="phone-landscape">
-                    <h4>Contact Us</h4>
-                    <p>3rd Floor, <br>90 Westbourne Grove, <br> London, W2 5RT</p>
-                    <p><span>Amy Witchalls</span> <br>Executive Assistant <br><a href="tel:+447792176959" onClick="ga('send', 'event', 'lead', 'amyphone', 'contactpage', 10, {'NonInteraction': 1});">+44 (0) 7792 176 959 </a> <br><a href="mailto:info@princedalepartners.com">info@princedalepartners.com</a></p>					
-                </div><!-- phone-landscape -->
+<div class='row'>
+    <div class='col-12'>
+        <div class="hero contact-image">
+            <div class='row'>
+                <div class='col-12 col-md-6 offset-md-3 cover-text'>
+                    <h1>Get connected</h1>
+                    <p>Give us a call if we can help you with related advice or help with key digital leadership hires.</p>
+                    <p>
+                    +44 (0) 203 865 1701 <br><a href="mailto:info@princedalepartners.com" onClick="ga('send', 'event', 'lead', 'amyemail', 'contactpage', 5, {'NonInteraction': 1});">info@princedalepartners.com</a>
+                </p>
+                <p>3rd Floor, <br>90 Westbourne Grove, <br> London, W2 5RT</p>
+                </div>
             </div>
-            <a href="https://www.google.co.uk/maps/place/90+Westbourne+Grove,+London+W2+5RT/@51.5153603,-0.202371,15z/data=!4m5!3m4!1s0x48761001f9736d07:0x206de62369679be8!8m2!3d51.515357!4d-0.1936109" target="_blank" class="button">&gt; View our location on Google Maps</a>
-        </div>
-        
-    </div><!-- hero -->
+            <div class='row'>
+                <div class='col-12 col-md-6 offset-md-3 cover-text'>
+                    <a href="https://www.google.co.uk/maps/place/90+Westbourne+Grove,+London+W2+5RT/@51.5153603,-0.202371,15z/data=!4m5!3m4!1s0x48761001f9736d07:0x206de62369679be8!8m2!3d51.515357!4d-0.1936109" target="_blank" class="button">&gt; View our location on Google Maps</a>
+                </div>
+            </div>
+        </div><!-- hero -->
+    </div>
+</div>
 @endsection
