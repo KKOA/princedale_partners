ccf95ce2c631828199c5ec7d94fad0eb8ff26c33
keith(MacBookPro) <ka33145@googlemail.com>
2018-06-06 18:01:32 +0100

    Updated home page structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,109 +8,91 @@
 @endsection
 
 @section('main')
-<div class="hero home-image">
-			
-        <div class="section group">
-            <div class="col span_3">
-            <h1>Digital Leadership Headhunters</h1>	
-            <p>We focus solely on digital leadership appointments across Europe. We help clients appoint business critical digital talent to lead growth and transformation for established and SME/growth companies on both a permanent and interim basis.</p>
-            <a href="about-us" class="button">&gt; Find Out How</a>
-            </div>
+    <div class='row'>
+        <div class='col-12'> 
+            <div class="hero home-image">
+                <div class="row">
+                    <div class='col-12 col-md-8 offset-md-2 cover-text'>
+                    <h1>Digital Leadership Headhunters</h1>	
+                    <p>We focus solely on digital leadership appointments across Europe. We help clients appoint business critical digital talent to lead growth and transformation for established and SME/growth companies on both a permanent and interim basis.</p>
+                    <a href="about-us" class="button">&gt; Find Out How</a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class='col-6 offset-3'>
+                        <h2>Example Searches</h2>
+                        <p class='d-block d-md-none'>Click on a logo to read a full case study</p>
+                    </div>
+                </div>
+
+                <div class='row'>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/coca-cola.html">
+                                <img src="img/logo-coca-cola.png" alt="Coca Cola Logo">
+                            </a>
+                            <a href="case-studies/coca-cola.html" class='d-none d-md-inline'>
+                                <span>Digital Director</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/thomas-cook.html">
+                                <img src="img/logo-thomas-cook.png" alt="Thomas Cook Logo">
+                            </a>
+                            <a href="case-studies/thomas-cook.html" class='d-none d-md-inline'>
+                                <span>Group Head, Digital Experience</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/holland-barrett.html">
+                                <img src="img/logo-holland-barrett.png" alt="Holland and Barrett Logo">
+                            </a>
+                            <a href="case-studies/holland-barrett.html" class='d-none d-md-inline'>
+                                <span>Director, Digital/Ecommerce</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                            <a href="case-studies/body-shop.html">
+                                <img src="img/logo-body-shop.png" alt="Body Shop Logo">
+                            </a>
+                            <a href="case-studies/body-shop.html" class='d-none d-md-inline'>
+                                <span>Chief Digital Officer</span><br>&gt; Read full case study
+                            </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/zalando.html">
+                            <img src="img/logo-zalando.png" alt="Zalando Logo">
+                        </a>
+                        <a href="case-studies/zalando.html" class='d-none d-md-inline'>
+                            <span>UK Country Manager</span><br>&gt; Read full case study
+                        </a>
+                    </div>	
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/photobox.html">
+                            <img src="img/logo-photobox.png" alt="Photobox Logo">
+                        </a>
+                        <a href="case-studies/photobox.html" class='d-none d-md-inline'>
+                            <span>Director, Customer Insight</span><br>&gt; Read full case study
+                        </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/secret-escapes.html">
+                            <img src="img/logo-secret-escapes.png" alt="Secret Escapes Logo">
+                        </a>
+                        <a href="case-studies/secret-escapes.html" class='d-none d-md-inline'>
+                            <span>Merchandising Director</span><br>&gt; Read full case study
+                        </a>
+                    </div>
+                    <div class='col-6 col-md-4 col-lg-3'>
+                        <a href="case-studies/audio-network.html">
+                            <img src="img/logo-audio-network.png" alt="The Audio Network Logo">
+                        </a>
+                        <a href="case-studies/audio-network.html" class='d-none d-md-inline'>
+                            <span>Product Director</span><br>&gt; Read full case study
+                        </a>
+                    </div>			
+                </div> 
+            </div>     
         </div>
-        
-        <div class="desktop-logos">
-        
-            <div class="section group">
-                <h2>Example Searches</h2>
-                <p class="case-study-line-hide">Click on a logo to read a full case study</p>
-            </div>
-            
-            <div class="section group logos">
-                <div>
-                    <a href="case-studies/coca-cola.html"><img src="img/logo-coca-cola.png" alt="Coca Cola Logo"></a>
-                    <a href="case-studies/coca-cola.html" class="case-study-title-hide"><span>Digital Director</span><br>&gt; Read full case study</a>
-                </div>	
-                <div class="logo-adjust">
-                    <a href="case-studies/thomas-cook.html"><img src="img/logo-thomas-cook.png" alt="Thomas Cook Logo"></a>
-                    <a href="case-studies/thomas-cook.html" class="case-study-title-hide"><span>Group Head, Digital Experience</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/holland-barrett.html"><img src="img/logo-holland-barrett.png" alt="Holland and Barrett Logo"></a>
-                    <a href="case-studies/holland-barrett.html" class="case-study-title-hide"><span>Director, Digital/Ecommerce</span><br>&gt; Read full case study</a>
-                </div>	
-                <div>
-                    <a href="case-studies/body-shop.html"><img src="img/logo-body-shop.png" alt="Body Shop Logo"></a>
-                    <a href="case-studies/body-shop.html" class="case-study-title-hide"><span>Chief Digital Officer</span><br>&gt; Read full case study</a>
-                </div>											
-            </div><!-- logo-line-1 -->
-            
-            <div class="section group logos">
-                <div>
-                    <a href="case-studies/zalando.html"><img src="img/logo-zalando.png" alt="Zalando Logo"></a>
-                    <a href="case-studies/zalando.html" class="case-study-title-hide"><span>UK Country Manager</span><br>&gt; Read full case study</a>
-                </div>	
-                <div class="logo-adjust">
-                    <a href="case-studies/photobox.html"><img src="img/logo-photobox.png" alt="Photobox Logo"></a>
-                    <a href="case-studies/photobox.html" class="case-study-title-hide"><span>Director, Customer Insight</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/secret-escapes.html"><img src="img/logo-secret-escapes.png" alt="Secret Escapes Logo"></a>
-                    <a href="case-studies/secret-escapes.html" class="case-study-title-hide"><span>Merchandising Director</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/audio-network.html"><img src="img/logo-audio-network.png" alt="The Audio Network Logo"></a>
-                    <a href="case-studies/audio-network.html" class="case-study-title-hide"><span>Product Director</span><br>&gt; Read full case study</a>
-                </div>								
-            </div><!-- logo-line-2 -->			
-            
-        </div><!-- desktop-logos -->			
-        
-        
-    </div><!-- hero -->	
-    
-        <div class="phone-logos">
-        
-            <div class="section group">
-                <h2>Example Searches</h2>
-            </div>
-            
-            <div class="section group logos">
-                <div>
-                    <a href="case-studies/coca-cola.html"><img src="img/logo-coca-cola.png" alt="Coca Cola Logo"></a>
-                    <a href="case-studies/coca-cola.html"><span>Digital Director</span><br>&gt; Read full case study</a>
-                </div>	
-                <div class="logo-adjust">
-                    <a href="case-studies/thomas-cook.html"><img src="img/logo-thomas-cook.png" alt="Thomas Cook Logo"></a>
-                    <a href="case-studies/thomas-cook.html"><span>Group Head, Digital Experience</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/holland-barrett.html"><img src="img/logo-holland-barrett.png" alt="Holland and Barrett Logo"></a>
-                    <a href="case-studies/holland-barrett.html"><span>Director, Digital/Ecommerce</span><br>&gt; Read full case study</a>
-                </div>	
-                <div>
-                    <a href="case-studies/body-shop.html"><img src="img/logo-body-shop.png" alt="Body Shop Logo"></a>
-                    <a href="case-studies/body-shop.html"><span>Chief Digital Officer</span><br>&gt; Read full case study</a>
-                </div>											
-            </div><!-- logo-line-1 -->
-            
-            <div class="section group logos">
-                <div>
-                    <a href="case-studies/zalando.html"><img src="img/logo-zalando.png" alt="Zalando Logo"></a>
-                    <a href="case-studies/zalando.html"><span>UK Country Manager</span><br>&gt; Read full case study</a>
-                </div>	
-                <div class="logo-adjust">
-                    <a href="case-studies/photobox.html"><img src="img/logo-photobox.png" alt="Photobox Logo"></a>
-                    <a href="case-studies/photobox.html"><span>Director, Customer Insight</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/secret-escapes.html"><img src="img/logo-secret-escapes.png" alt="Secret Escapes Logo"></a>
-                    <a href="case-studies/secret-escapes.html"><span>Merchandising Director</span><br>&gt; Read full case study</a>
-                </div>
-                <div>
-                    <a href="case-studies/audio-network.html"><img src="img/logo-audio-network.png" alt="The Audio Network Logo"></a>
-                    <a href="case-studies/audio-network.html"><span>Product Director</span><br>&gt; Read full case study</a>
-                </div>								
-            </div><!-- logo-line-2 -->			
-            
-        </div><!-- hero-logo-wrap -->
+    </div>	
 @endsection
