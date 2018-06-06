0fe69f7d77357e1067c83796501e8e563c01d01c
keith(MacBookPro) <ka33145@googlemail.com>
2018-06-06 18:13:57 +0100

    Added updated user migration and Model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        // 'name',
+        'first_name','last_name', 'alias','email', 'password',
+        'address1','address2','city','county','postcode',
+        'telephone',
+        'cv',
+        'photo'
     ];
 
     /**
@@ -26,4 +31,94 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function not_blank($element)
+    {
+        $element = trim($element);
+        return (strlen($element) > 0 );
+    }
+
+    public function full_name()
+    {
+        return $this->first_name.' '.$this->last_name;
+    }
+
+    public function full_address()
+    {
+        $parts = [
+            $this->address1,
+            $this->address2,
+            $this->city,
+            $this->county,
+            $this->postcode
+        ];
+        $address = array_filter($parts, [$this,'not_blank']);
+        return implode(', ',$address);
+    }
+
+
+    // DEFINE Mutators --------------------------------------------------
+    // Getter
+
+    
+    public function getFirstNameAttribute($value)
+    {
+        return ucwords($value);
+    }
+    public function getLastNameAttribute($value)
+    {
+        return ucwords($value);
+    }
+    public function getAddress1Attribute($value)
+    {
+        return ucwords($value);
+    }
+    public function getAddress2Attribute($value)
+    {
+        return ucwords($value);
+    }
+    public function getCityAttribute($value)
+    {
+        return ucwords($value);
+    }
+    public function getCountyAttribute($value)
+    {
+        return ucwords($value);
+    }
+    public function getPostcodeAttribute($value)
+    {
+        return strtoupper($value);
+    }
+    // Setter
+    public function setFirstNameAttribute($value)
+    {
+        $this->attributes['first_name'] = ucwords($value);
+    }
+    public function setLastAttribute($value)
+    {
+        $this->attributes['last_name'] = ucwords($value);
+    }
+    public function setAddress1Attribute($value)
+    {
+        $this->attributes['address1'] = ucwords($value);
+    }
+    public function setAddress2Attribute($value)
+    {
+        $this->attributes['address2'] = ucwords($value);
+    }
+    public function setCityAttribute($value)
+    {
+        $this->attributes['city'] = ucwords($value);
+    }
+    public function setCountyAttribute($value)
+    {
+        $this->attributes['county'] = ucwords($value);
+    }
+    public function setPostcodeAttribute($value)
+    {
+        $this->attributes['postcode'] = strtoupper($value);
+    }
+
+
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            // $table->string('name');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('alias', 20)->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address1', 100)->nullable();
+            $table->string('address2', 100)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->string('county', 100)->nullable();
+            $table->string('postcode', 10)->nullable();
+            $table->string('telephone', 12)->nullable();
+            $table->string('cv')->nullable();
+            $table->string('photo')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
